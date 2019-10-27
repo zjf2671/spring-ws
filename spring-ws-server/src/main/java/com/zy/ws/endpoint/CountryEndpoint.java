077b7f513f4916ddc0eb2b96138d42acc5baa598
harry <zhangjianfeng>
2019-10-27 22:38:04 +0800

    fix: add test data

--- spring-ws-server/src/main/java/com/zy/ws/endpoint/CountryEndpoint.java
+++ spring-ws-server/src/main/java/com/zy/ws/endpoint/CountryEndpoint.java
@@ -4,6 +4,8 @@ import com.zy.ws.Country;
 import com.zy.ws.Currency;
 import com.zy.ws.GetCountryRequest;
 import com.zy.ws.GetCountryResponse;
+import com.zy.ws.data.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -15,6 +17,14 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class CountryEndpoint {
     private static final String NAMESPACE_URI = "http://www.zy.com/ws";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
     @ResponsePayload
     public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
@@ -25,6 +35,7 @@ public class CountryEndpoint {
         poland.setCurrency(Currency.PLN);
         poland.setPopulation(38186860);
         response.setCountry(poland);
+//        response.setCountry(countryRepository.findCountry(request.getName()));
         return response;
     }
 }
